6a45d7e6f9a205e86c1a608a95220d203ece487d
Libikk <libikpl@gmail.com>
2018-09-18 20:56:40 +0100

    es6 fat arrow functions added

--- src/components/Layout/containers/Footer.js
+++ src/components/Layout/containers/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 export default class Footer extends React.Component {
+  test = () => {
+    console.log('this is test');
+  }
   render() {
     return (
-      <div style={{ background: 'blue' }}>
+      <div style={{ background: 'blue', color: 'red' }}>
+        <button onClick={this.test}>consol</button>
           THIS IS Footer
+          aasdasdasdasdasdassaaaaja pierdole gg
       </div>
     );
   }
--- src/components/Layout/containers/Navbar.js
+++ src/components/Layout/containers/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <ul>
-      <li><Link href="/home" to="/home">About</Link></li>
+      <li><Link href="/" to="/">Home</Link></li>
       <li><Link href="/aboutme" to="/aboutme">About Me</Link></li>
       <li><Link href="/transformations" to="/transformations">Transformations</Link></li>
     </ul>
