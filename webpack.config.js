7962824f500809415eb913655024a9ffe1e965fa
Libikk <libikpl@gmail.com>
2018-12-08 17:06:08 +0000

    integration with ie 11

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 const browserConfig = {
-  entry: './src/browser/index.js',
+  entry: ['@babel/polyfill', './src/browser/index.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
@@ -33,7 +33,7 @@ const browserConfig = {
 };
 
 const serverConfig = {
-  entry: './src/server/index.js',
+  entry: ['@babel/polyfill', './src/server/index.js'],
   target: 'node',
   externals: [nodeExternals()],
   output: {
