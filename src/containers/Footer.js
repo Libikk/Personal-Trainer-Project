2e5501ca261860a58f593c75d75d940ac66c55ca
Libikk <libikpl@gmail.com>
2019-01-05 20:12:27 +0000

    footer styles

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import '../styles/footer.scss';
 
+import FacebookIcon from '../icons/facebook.svg';
+import TwitterIcon from '../icons/twitter.svg';
+import InstagramIcon from '../icons/instagram.svg';
+import YoutubeIcon from '../icons/youtube.svg';
+import EmailIcon from '../icons/email.svg';
+import PhoneIcon from '../icons/phone.svg';
+import LocationIcon from '../icons/location.svg';
+
+
 export default class Footer extends React.Component {
   render() {
     return (
-      <div className="footer-container">
-          THIS IS Footerr
-      </div>
+      <footer className="footer">
+        <div className="footer__content">
+          <section className="content__contact">
+            <span><PhoneIcon /> (0) 7704438594</span>
+            <span><LocationIcon /> The Gym London Kingsbury, 632-640 Kingsbury Road, London NW9 9HN</span>
+            <span><EmailIcon /> developer@kregielmarek.com</span>
+          </section>
+          <div className="thin-line" />
+          <p className="content__icons">
+            <FacebookIcon />
+            <InstagramIcon />
+            <TwitterIcon />
+            <YoutubeIcon />
+          </p>
+          <p className="content__copyrights" >© 2018 <a href="https://github.com/Libikk">Marek Kregiel</a> All Rights Reserved.</p>
+        </div>
+      </footer>
     );
   }
 }
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,7 +2,12 @@ import LoadingBar from 'react-redux-loading-bar';
 import React from 'react';
 import Navbar from './Navbar';
 import '../styles/header.scss';
-import hujj from '../images/logo_black.png';
+import hujj from '../images/banana.png';
+
+import FacebookIcon from '../icons/facebook.svg';
+import TwitterIcon from '../icons/twitter.svg';
+import InstagramIcon from '../icons/instagram.svg';
+import YoutubeIcon from '../icons/youtube.svg';
 
 
 export default class Header extends React.Component {
@@ -15,10 +20,10 @@ export default class Header extends React.Component {
         </div>
         <Navbar location={this.props.location} />
         <div className="header__social-media">
-          <div>FB</div>
-          <div>INSTA</div>
-          <div>YT</div>
-          <div>TWITT</div>
+          <FacebookIcon />
+          <TwitterIcon />
+          <InstagramIcon />
+          <YoutubeIcon />
         </div>
       </div>
     );
