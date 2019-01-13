9b6ca2cc7049f6133fdd6241aec0117cb553e399
Libikk <libikpl@gmail.com>
2019-01-13 18:37:27 +0000

    added hamburger menu for mobiles

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,12 +4,6 @@ import Navbar from './Navbar';
 import '../styles/header.scss';
 import hujj from '../images/banana.png';
 
-import FacebookIcon from '../icons/facebook.svg';
-import TwitterIcon from '../icons/twitter.svg';
-import InstagramIcon from '../icons/instagram.svg';
-import YoutubeIcon from '../icons/youtube.svg';
-
-
 export default class Header extends React.Component {
   render() {
     return (
@@ -19,12 +13,6 @@ export default class Header extends React.Component {
           <img alt="ddd" src={hujj} />
         </div>
         <Navbar location={this.props.location} />
-        <div className="header__social-media">
-          <FacebookIcon />
-          <TwitterIcon />
-          <InstagramIcon />
-          <YoutubeIcon />
-        </div>
       </div>
     );
   }
