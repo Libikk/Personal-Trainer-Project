9fa8d78a722608ef2737fe59213181419cdd65b3
Libikk <libikpl@gmail.com>
2019-02-10 19:13:28 +0000

    transformations init page

--- src/components/Transformations.js
+++ src/components/Transformations.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import '../styles/transformations.scss';
 import { hideLoadingBar, showLoadingBar } from '../browser/actions';
-import huj from '../images/owl.jpg';
-import hujj from '../images/ow2l.png';
+import transformationList from '../../appPageFeed/transformationList.json';
+import resultsGuaranteeImage from '../images/guarantee_results.png';
+import utils from '../utils';
 
 class Transformations extends React.Component {
-  componentWillMount = () => {
-    this.props.showLoadingBar();
-    axios.get('http://localhost:3000/api/instagramProfile')
-      .then((e) => {
-        console.log(e.status);
-        this.props.hideLoadingBar();
-      });
+  state = {
+    transformationLimitDisplay: 1,
+  }
+
+  loadMoreTransformations = () => {
+    this.setState({ transformationLimitDisplay: this.state.transformationLimitDisplay + 1 });
   }
   render() {
+    const { transformationLimitDisplay } = this.state;
+    const transformationsListRendered = transformationList.slice(0, transformationLimitDisplay).map((e) => {
+      const image = require(`../images/${e.images}`);// eslint-disable-line
+      return (
+        <div className="single-transformation" key={e.personName}>
+          <div className="transformation__image-wrapper">
+            <img alt="awl" src={image} />
+            <img alt="huj" src={resultsGuaranteeImage} />
+            {/* <div className="promo">this is under photo</div> */}
+          </div>
+          <div className="transformation__details">
+            <span className="details__person-name">{e.personName} <sup>{utils.getTextUploadTime(e.uploadDate)}</sup></span>
+            <div className="details__about-person">{`"${e.aboutPerson}"`}</div>
+          </div>
+        </div>);
+    });
     return (
-      <div>
-        <img alt="awl" src={hujj} />
-        <img alt="awl" src={huj} />
+      <div className="transformations-container">
+        <div className="transformations__wrapper">
+          {transformationsListRendered}
+        </div>
+        { transformationLimitDisplay >= transformationList.length ? null : <span className="primary-button load-more" onClick={() => this.loadMoreTransformations()}>Load more</span>}
       </div>
     );
   }
